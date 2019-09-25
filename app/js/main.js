c5f1e2a22ff6dec7d6b870b05255aba1f827f516
DrevniyMagistr <sasha.klishch@gmail.com>
2019-09-26 01:04:51 +0300

    added list style for new-items

--- app/js/main.js
+++ app/js/main.js
@@ -9,13 +9,10 @@ $(function () {
   });
 
   $('.product-slider__inner').slick({
-
-    infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
     arrows: false,
-
   });
 
   $(".js-range-slider").ionRangeSlider({
@@ -25,6 +22,17 @@ $(function () {
     from: 0,
     to: 600,
     prefix: "$"
+  });
+
+  $('.icon-th-list').on('click', function(){
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+  $('.icon-th-large').on('click', function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
   });
 
   var mixer = mixitup('.products__inner-box');
